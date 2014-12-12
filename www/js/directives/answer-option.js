9a0ef62191c47a3e012f156c14fd5d040a91c9db
seescode <seesyong@gmail.com>
2014-12-11 22:54:18 -0500

    Making it easier to touch the buttons and view stuff on the guess kanji page

--- www/js/directives/answer-option.js
+++ www/js/directives/answer-option.js
@@ -4,7 +4,6 @@ angular.module('japaneseHelperApp')
   .directive('answerOption', function(kanjiHelper) {
     return {
         restrict: 'A',
-        replace: true,
         template: '<div ng-click="click()">{{answerText}}</div>',
         scope: { 
             answerText: '@',
